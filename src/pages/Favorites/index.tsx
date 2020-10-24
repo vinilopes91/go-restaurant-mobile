6f3cbb35a6299f7e0dff3df3e9d1831465fecdec
vinilopes91 <vinilopes13@hotmail.com>
2020-10-24 04:04:34 -0300

    resolucao

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -32,7 +32,14 @@ const Favorites: React.FC = () => {
 
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
-      // Load favorite foods from api
+      const response = await api.get('/favorites');
+
+      const formattedFavorites = response.data.map((favorite: Food) => ({
+        ...favorite,
+        formatedPrice: formatValue(favorite.price),
+      }));
+
+      setFavorites(formattedFavorites);
     }
 
     loadFavorites();
--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -32,7 +32,12 @@ const Orders: React.FC = () => {
 
   useEffect(() => {
     async function loadOrders(): Promise<void> {
-      // Load orders from API
+      const response = await api.get('/orders');
+      const formattedOrders = response.data.map((order: Food) => ({
+        ...order,
+        formatedPrice: formatValue(order.price),
+      }));
+      setOrders(formattedOrders);
     }
 
     loadOrders();
